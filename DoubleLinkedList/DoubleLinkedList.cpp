cc8cf09466f0489e24d34ec72685ce508d067a30
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-25 19:41:53 +0700

    menambahkan metode

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -31,5 +31,28 @@ DoubleLinkedList::DoubleLinkedList() {
 }
 
 void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student :";
+	cin >> nm;
+	node* newNode = new node(); // step 1
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm; // step 2
+
+	// insert a node in the beginning of a doubly - linked list
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;  // step 3
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
 
 }
