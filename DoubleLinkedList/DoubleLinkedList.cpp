5ea4c004cff15e27e81ad6a4265b5014bbe175a5
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-25 19:44:03 +0700

    constructor

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -73,3 +73,7 @@ void DoubleLinkedList::addNode() {
 		current->next->prev = newNode; // step 6
 	current->next = newNode; //step 7
 }
+
+bool DoubleLinkedList::search(int rollNo, node** previous, node** current) {
+
+}
