2b514e1356b63ef901f8f1310ea0a41c0366e6ec
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-25 19:38:51 +0700

    menabhkan deklarasi private dan public

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -12,5 +12,16 @@ public:
 };
 
 class DoubleLinkedList {
-
+private:
+	node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpety();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void SearchData();
 };
